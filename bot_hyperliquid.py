7879ec6ee168709572f409cd4b7f5f4dcc1de416
AWSspanishmaster <miguelangel.nds@gmail.com>
2025-05-25 18:06:50 +0200

    Update bot_hyperliquid.py

--- bot_hyperliquid.py
+++ bot_hyperliquid.py
@@ -1,3 +1,20 @@
+import os
+from threading import Thread
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Bot is running')
+
+def run_server():
+    port = int(os.environ.get("PORT", 8000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    server.serve_forever()
+
+# Lanzar el servidor en un hilo separado para que no bloquee
+Thread(target=run_server, daemon=True).start()
 import asyncio
 import json
 import websockets
